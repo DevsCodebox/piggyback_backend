a1a0438dd308971141fc9379adc9bf25399ddb52
Sawim Zargar <sazargar@uwaterloo.ca>
2017-03-14 02:02:18 -0400

    Account creation and password change

--- account.py
+++ account.py
@@ -1,7 +1,53 @@
 from mongo_connection import client
+import json
 
 class Account():
     def get_user(self, user_id):
         user_db = client.user
         user = user_db.find_one({"user_id": user_id})
         return user
+    
+    def create_account(self, first_name, last_name, user_name, email, dob, password, password_confirm):
+	# check for user_name already existing
+	if find_user(user_name):
+	    return False
+	# check if password and password_confirm match
+	if not compare_passwords(password, password_confirm):
+	    return False
+	# then create the account
+        data = {}
+	data['first_name'] = first_name
+	data['last_name'] = last_name
+	data['user_name'] = user_name
+	data['email'] = email
+	data['dob'] = dob
+	data['password'] = password
+	
+	json_data = json.dumps(data)
+	users = client.users
+
+	result = users.insert_one(json_data)
+	return True
+
+    def find_user(user_name):
+	users = client.users
+	for user in users.find({"user_name": user_name}):
+	    return True
+	return False
+
+    def compare_passwords(password, password_confirm):
+	if (password != password_confirm):
+	    return False
+	return True
+
+    def change_password(user_name, old_password, new_password, password_confirm):
+	if not compare_passwords(new_password, password_confirm):
+	    return False
+	users = client.users
+	users.update(
+	    {"user_name": user_name},
+		{$set: "password": new_password}
+	    }
+	)
+	
+	return True
