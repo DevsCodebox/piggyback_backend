0cd7590f3911e2d4b653a3c9ed6c519f51a618b8
Sawim Zargar <sazargar@uwaterloo.ca>
2017-03-14 02:20:34 -0400

    Fix tab spacing

--- account.py
+++ account.py
@@ -2,20 +2,20 @@ from mongo_connection import client
 import json
 
 class Account():
-    def get_user(self, user_id):
-        user_db = client.user
-        user = user_db.find_one({"user_id": user_id})
-        return user
+	def get_user(self, user_id):
+		user_db = client.user
+		user = user_db.find_one({"user_id": user_id})
+		return user
     
-    def create_account(self, first_name, last_name, user_name, email, date_of_birth, password, password_confirm):
+	def create_account(self, first_name, last_name, user_name, email, date_of_birth, password, password_confirm):
 	# check for user_name already existing
 	if self.find_user(user_name):
-	    return False
+		return False
 	# check if password and password_confirm match
 	if not self.compare_passwords(password, password_confirm):
-	    return False
+		return False
 	# then create the account
-        data = {}
+		data = {}
 	data['first_name'] = first_name
 	data['last_name'] = last_name
 	data['user_name'] = user_name
@@ -29,25 +29,25 @@ class Account():
 	result = users.insert_one(json_data)
 	return True
 
-    def find_user(self, user_name):
+	def find_user(self, user_name):
 	users = client.users
 	for user in users.find({"user_name": user_name}):
-	    return True
+		return True
 	return False
 
-    def compare_passwords(self, password, password_confirm):
-	if (password != password_confirm):
-	    return False
-	return True
+	def compare_passwords(self, password, password_confirm):
+		if (password != password_confirm):
+	    	return False
+		return True
 
-    def change_password(self, user_name, old_password, new_password, password_confirm):
-	if not compare_passwords(new_password, password_confirm):
-	    return False
-	users = client.users
-	users.update(
-	    {"user_name": user_name},
-		{$set: "password": new_password}
-	    }
-	)
+	def change_password(self, user_name, old_password, new_password, password_confirm):
+		if not compare_passwords(new_password, password_confirm):
+	    	return False
+		users = client.users
+		users.update(
+			{"user_name": user_name},
+				{$set: "password": new_password}
+	    	}
+		)
 	
-	return True
+		return True
