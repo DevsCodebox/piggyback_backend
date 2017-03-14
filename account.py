b9aa0afb890e2556ff2037433243a8002f84bdd3
Sawim Zargar <sazargar@uwaterloo.ca>
2017-03-14 02:16:58 -0400

    Add self to account.py

--- account.py
+++ account.py
@@ -9,10 +9,10 @@ class Account():
     
     def create_account(self, first_name, last_name, user_name, email, date_of_birth, password, password_confirm):
 	# check for user_name already existing
-	if find_user(user_name):
+	if self.find_user(user_name):
 	    return False
 	# check if password and password_confirm match
-	if not compare_passwords(password, password_confirm):
+	if not self.compare_passwords(password, password_confirm):
 	    return False
 	# then create the account
         data = {}
@@ -29,18 +29,18 @@ class Account():
 	result = users.insert_one(json_data)
 	return True
 
-    def find_user(user_name):
+    def find_user(self, user_name):
 	users = client.users
 	for user in users.find({"user_name": user_name}):
 	    return True
 	return False
 
-    def compare_passwords(password, password_confirm):
+    def compare_passwords(self, password, password_confirm):
 	if (password != password_confirm):
 	    return False
 	return True
 
-    def change_password(user_name, old_password, new_password, password_confirm):
+    def change_password(self, user_name, old_password, new_password, password_confirm):
 	if not compare_passwords(new_password, password_confirm):
 	    return False
 	users = client.users
