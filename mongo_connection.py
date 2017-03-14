e965899d0bf29b2ef1c01bcf2dfd7846addad4bc
Sawim Zargar <sazargar@uwaterloo.ca>
2017-03-13 23:48:39 -0400

    Casting port to a string

--- mongo_connection.py
+++ mongo_connection.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 ADDRESS = 'some_string'
 PORT = some_number
-DB_URI = 'mongodb://' + ADDRESS + ':' + PORT
+DB_URI = 'mongodb://' + ADDRESS + ':' + str(PORT)
 
 def get_connection_info():
     client = MongoClient(DB_URI)
