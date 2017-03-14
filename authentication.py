d22adc12270df297e8b894cb5baa22238ab00366
Sawim Zargar <sazargar@uwaterloo.ca>
2017-03-14 00:56:10 -0400

    Simplify account verification to true and false

--- authentication.py
+++ authentication.py
@@ -4,5 +4,5 @@ class Authentication:
     def check_login(self, user_name, password):
         users = client.users
         for user in users.find({"user_name": user_name, "password": password}):
-            return user
-	return None
+            return True
+	return False
