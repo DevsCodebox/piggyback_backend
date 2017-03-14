4d9632ff8e371e7a37a13f55421916d5c6624bad
Sawim Zargar <sazargar@uwaterloo.ca>
2017-03-14 00:50:08 -0400

    Cleaner authentication check

--- authentication.py
+++ authentication.py
@@ -1,13 +1,8 @@
 from mongo_connection import client
 
-found_users = []
-
 class Authentication:
     def check_login(self, user_name, password):
         users = client.users
         for user in users.find({"user_name": user_name, "password": password}):
-            found_users.append(user)
-	if (len(found_users) == 1):
             return user
-	else:
-	    return None
+	return None
