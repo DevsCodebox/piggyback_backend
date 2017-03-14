2084355afcb39047dca08fd48f2ee1f2048c7c85
Hani Mir <hanimir@Hanis-MacBook-Pro.local>
2017-03-14 01:20:28 -0400

    fix get_user in account

--- account.py
+++ account.py
@@ -3,5 +3,5 @@ from mongo_connection import client
 class account():
     def get_user(self, user_id):
         user_db = client.user
-        user = user_db.find({"user_id": user_id})
-        return user[0]
+        user = user_db.find_one({"user_id": user_id})
+        return user
