967334659cc5cc4cc202504b0d94d9990d4c3f13
Sawim Zargar <sazargar@uwaterloo.ca>
2017-03-13 23:50:32 -0400

    Removing function definition

--- mongo_connection.py
+++ mongo_connection.py
@@ -3,7 +3,4 @@ from pymongo import MongoClient
 ADDRESS = 'some_string'
 PORT = some_number
 DB_URI = 'mongodb://' + ADDRESS + ':' + str(PORT)
-
-def get_connection_info():
-    client = MongoClient(DB_URI)
-    return client
+client = MongoClient(DB_URI)
