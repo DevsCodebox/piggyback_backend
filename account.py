6053ca2b3fb4f19b228001685a9f04227873b675
DOMINIC LI <dphli@uwaterloo.ca>
2017-03-15 19:47:40 -0400

    remove env from repo, fix tabs to spaces

--- account.py
+++ account.py
@@ -2,84 +2,84 @@ from mongo_connection import client
 import json
 
 class Account():
-	@staticmethod
-	def get_user(user_name):
-		user_db = client.users
-		user = user_db.find_one({'user_name': user_name})
-		return user
+    @staticmethod
+    def get_user(user_name):
+        user_db = client.users
+        user = user_db.find_one({'user_name': user_name})
+        return user
 
-	@staticmethod
-	def create_account(first_name, last_name, user_name, email, date_of_birth, password, password_confirm):
-		"""
-		curl -H "Content-Type: application/json" -X POST localhost:5000/api/createUser -d '{"first_name":"test2",
-		 "last_name":"test", "email":"email",
+    @staticmethod
+    def create_account(first_name, last_name, user_name, email, date_of_birth, password, password_confirm):
+        """
+        curl -H "Content-Type: application/json" -X POST localhost:5000/api/createUser -d '{"first_name":"test2",
+         "last_name":"test", "email":"email",
         "date_of_birth":"temp",
         "password":"temp",
         "Password_confirm":"temp",
         "user_name":"wtf_a"}'
 
-		:param first_name:
-		:param last_name:
-		:param user_name:
-		:param email:
-		:param date_of_birth:
-		:param password:
-		:param password_confirm:
-		:return:
-		"""
-		# check for user_name already existing
-		if Account.find_user(user_name):
-			return False
-		# check if password and password_confirm match
-		if password != password_confirm:
-			return False
-		# then create the account
-		data = {}
-		data['first_name'] = first_name
-		data['last_name'] = last_name
-		data['user_name'] = user_name
-		data['email'] = email
-		data['date_of_birth'] = date_of_birth
-		data['password'] = password
-		data['credits'] = 0
+        :param first_name:
+        :param last_name:
+        :param user_name:
+        :param email:
+        :param date_of_birth:
+        :param password:
+        :param password_confirm:
+        :return:
+        """
+        # check for user_name already existing
+        if Account.find_user(user_name):
+            return False
+        # check if password and password_confirm match
+        if password != password_confirm:
+            return False
+        # then create the account
+        data = {}
+        data['first_name'] = first_name
+        data['last_name'] = last_name
+        data['user_name'] = user_name
+        data['email'] = email
+        data['date_of_birth'] = date_of_birth
+        data['password'] = password
+        data['credits'] = 0
 
-		users = client.users
+        users = client.users
 
-		users.insert_one(data)
-		return True
+        users.insert_one(data)
+        return True
 
-	@staticmethod
-	def find_user( user_name):
-		users = client.users
-		temp = users.find_one({'user_name': user_name})
-		if temp:
-			return True
-		return False
+    @staticmethod
+    def find_user( user_name):
+        users = client.users
+        temp = users.find_one({'user_name': user_name})
+        if temp:
+            return True
+        return False
 
 
-	@staticmethod
-	def change_password(user_name, old_password, new_password, password_confirm):
-		if new_password != password_confirm:
-			return False
-		users = client.users
-		users.update(
-			{'user_name': user_name},
-			{'$set': {'password': new_password}}
-		)
-		return True
+    @staticmethod
+    def change_password(user_name, old_password, new_password, password_confirm):
+        if new_password != password_confirm:
+            return False
+        users = client.users
+        users.update(
+            {'user_name': user_name},
+            {'$set': {'password': new_password}}
+        )
+        return True
 
-	@staticmethod
-	def update_credits( user_name, credit_difference):
-		users = client.users
-		credit = 0
+    @staticmethod
+    def update_credits( user_name, credit_difference):
+        users = client.users
+        credit = 0
 
-		user = users.find_one({'user_name': user_name})
-		credit = user['credits'] + credit_difference
+        user = users.find_one({'user_name': user_name})
+        credit = user['credits'] + credit_difference
 
-		users.update(
-			{'user_name': user_name},
-			{'$set': {'credits': credit}}
-		)
-		
-		return True
+        users.update(
+            {'user_name': user_name},
+            {'$set': {'credits': credit}}
+        )
+        
+        return True
 
--- connections.py
+++ connections.py
@@ -2,41 +2,41 @@ from mongo_connection import client
 import json
 
 class Connections():
-	# function to return the max strength ssid from a list
-	@staticmethod
-	def strongest_ssid(ssid_list):
-		connections = client.connections
-		strongest = 0
-		best_ssid = 0
-		for ssid in ssid_list:
-			connection = connections.find_one({'ssid': ssid})
-			if connection['ssid_strength'] >= strongest:
-				strongest = connection['ssid_strength']
-				best_ssid = connection['ssid']
-		return best_ssid
+    # function to return the max strength ssid from a list
+    @staticmethod
+    def strongest_ssid(ssid_list):
+        connections = client.connections
+        strongest = 0
+        best_ssid = 0
+        for ssid in ssid_list:
+            connection = connections.find_one({'ssid': ssid})
+            if connection['ssid_strength'] >= strongest:
+                strongest = connection['ssid_strength']
+                best_ssid = connection['ssid']
+        return best_ssid
 
-	@staticmethod
-	def add_new_ssid(ssid, ssid_strength):
-		connections = client.connections
+    @staticmethod
+    def add_new_ssid(ssid, ssid_strength):
+        connections = client.connections
 
-		data = {}
-		data['ssid'] = ssid
-		data['ssid_strength'] = ssid_strength
-		temp = connections.find_one({'ssid': ssid})
-		if temp:
-			return False
+        data = {}
+        data['ssid'] = ssid
+        data['ssid_strength'] = ssid_strength
+        temp = connections.find_one({'ssid': ssid})
+        if temp:
+            return False
 
-		connections.insert_one(data)
+        connections.insert_one(data)
 
-		return True
+        return True
 
-	@staticmethod
-	def update_ssid_strength(ssid, ssid_strength):
-		connections = client.connections
+    @staticmethod
+    def update_ssid_strength(ssid, ssid_strength):
+        connections = client.connections
 
-		connections.update(
-			{'ssid': ssid},
-			{'$set': {'ssid_strength': ssid_strength}}
-		)
+        connections.update(
+            {'ssid': ssid},
+            {'$set': {'ssid_strength': ssid_strength}}
+        )
 
-		return True
+        return True
